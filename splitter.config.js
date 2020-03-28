620a7c9c1a8ca856a7d2167417b4e7f0702b20ae
Frank Behrens <fbehrens@gmail.com>
2020-03-28 18:44:04 +0100

    use babel/plugin-transform-modules-commonjs

--- splitter.config.js
+++ splitter.config.js
@@ -7,5 +7,8 @@ function resolve(relativePath) {
 module.exports = {  
   entry: resolve("src/fbVsCodeExt.fsproj"),
   outDir: resolve("out"),
+  babel: {
+    plugins: ["@babel/plugin-transform-modules-commonjs"]
+  },
   allFiles: true
 };
